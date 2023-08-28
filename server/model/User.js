196d7df4e2b1055ddf877ca50b6bef0cbd47f9f7
Chinmay Sabnis <sabnischinmay31@gmail.com>
2023-08-28 21:48:08 +0530

    changed authetication and autherization from jwt to passport.js

--- server/model/User.js
+++ server/model/User.js
@@ -1,45 +1,48 @@
-// import timespan from "jsonwebtoken/lib/timespan";
 import mongoose from "mongoose";
+import passportLocalMongoose from 'passport-local-mongoose';
 
 const userSchema = new mongoose.Schema({
-    firstName :{
-        type : String,
-        required:true,
-        min:3,
-        max:50
+    firstName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 50
     },
-    lastName :{
-        type : String,
-        required:true,
-        min:3,
-        max:50
+    lastName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 50
     },
-    email :{
-        type : String,
-        required:true,
-        unique:true,
-        max:50
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50
     },
-    password :{
-        type : String,
-        require:true,
-        min:5,
+    password: {
+        type: String,
+        require: true,
+        min: 5,
     },
-    picturePath :{
-        type : String,
-        default:""
+    picturePath: {
+        type: String,
+        default: ""
     },
-    friends:{
-        type : Array,
-        default : []
+    friends: {
+        type: Array,
+        default: []
     },
-    location : String,
-     occupation : String,
-     viewedProfiles : Number,
-     impressions : Number
+    location: String,
+    occupation: String,
+    viewedProfiles: Number,
+    impressions: Number
 },
-{timespan:true}
+    { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
-export default User;
+userSchema.plugin(passportLocalMongoose, {
+    usernameField: 'email'        // Specifing  that the "email" field should be treated as the username
+});
+
+export const User = mongoose.model('user', userSchema);
