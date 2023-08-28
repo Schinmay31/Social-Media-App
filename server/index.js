0febb80bb5a85bc4c78ae7adcf6001cb28038efb
Chinmay Sabnis <sabnischinmay31@gmail.com>
2023-08-29 02:30:50 +0530

    tp

--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+import session from "express-session";
+import passport from "./config/passportConfig.js";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/Auth.js";
-import session from "express-session";
-import passport from "./config/passportConfig.js";
+import userRoutes from "./routes/users.js";
+
 
 // Configrations
 const __filename = fileURLToPath(import.meta.url);
@@ -60,8 +62,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/social-media-app", { useNewUrlParser
    });
 
 
-//    Routes 
+//    Routes login
 app.use("/auth", authRoutes);
+
+app.use("/users",userRoutes);
 
 
 
